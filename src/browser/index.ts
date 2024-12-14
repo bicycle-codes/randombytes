432267f1686f354f29ced8529595e1fbb1e5a3e5
nichoth <nichoth@nichoth.com>
2024-12-14 13:40:16 -0800

    setup

---
+++ src/browser/index.ts
@@ -0,0 +1,32 @@
+'use strict'
+
+// limit of Crypto.getRandomValues()
+// https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+const MAX_BYTES = 65536
+
+// Node supports requesting up to this number of bytes
+// https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
+const MAX_UINT32 = 4294967295
+
+export function randomBytes (size:number):Uint8Array {
+    if (!crypto || !crypto.getRandomValues) {
+        throw new Error('Secure random number generation is not supported' +
+            'by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
+    }
+
+    if (size <= 0) {
+        throw new Error('Must pass a size greater than zero')
+    }
+
+    if (size > MAX_BYTES) throw new Error('requested too many random bytes')
+
+    if (size > MAX_UINT32) {
+        throw new RangeError('requested too many random bytes')
+    }
+
+    const arr = new Uint8Array(size)
+    const randoms = crypto.getRandomValues(arr)
+    return randoms
+}
+
+export default randomBytes
--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,9 @@
 import { test } from '@substrate-system/tapzero'
-import { example } from '../src/index.js'
+import randomBytes from '../src/browser/index.js'
 
 test('example', async t => {
     t.ok('ok', 'should be an example')
-    example()
+    const randoms = randomBytes(10)
+    t.ok(randoms instanceof Uint8Array,
+        'should return some random bytes as a buffer')
 })
